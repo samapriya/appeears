fdc4a4eef95f7ac2511226f6a31bc8d50cce9582
Samapriya Roy <samapriya.roy@gmail.com>
2022-10-16 02:40:36 -0500

    updated v0.0.1
    - added setup & reqs

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+from setuptools import find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="appeears",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/samapriya/appeears",
+    install_requires=[
+        "tqdm >= 4.56.0",
+        "requests >= 2.28.1",
+        "natsort >= 8.1.0"
+    ],
+    license="Apache 2.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    author="Samapriya Roy",
+    author_email="samapriya.roy@gmail.com",
+    description="Simple CLI for NASA AppEEARS API",
+    entry_points={
+        "console_scripts": [
+            "appeears=appeears.appeears:main",
+        ],
+    },
+)
