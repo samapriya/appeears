4c5cee3f39460d7c808198e707668a47615c356f
Samapriya Roy <samapriya.roy@gmail.com>
2022-10-18 02:13:42 -0400

    updated v0.0.2
    - added task submission
    - minor improvements

--- appeears/appeears.py
+++ appeears/appeears.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import getpass
 import json
 import os
@@ -9,12 +10,73 @@ from operator import itemgetter
 from os.path import expanduser
 
 import pkg_resources
+import pygeoj
 import requests
+from bs4 import BeautifulSoup
 from natsort import natsorted
 from tqdm import tqdm
 
-# from bs4 import BeautifulSoup as bs
-# from tenacity import retry, stop_after_attempt, wait_exponential
+
+class Solution:
+    def compareVersion(self, version1, version2):
+        versions1 = [int(v) for v in version1.split(".")]
+        versions2 = [int(v) for v in version2.split(".")]
+        for i in range(max(len(versions1), len(versions2))):
+            v1 = versions1[i] if i < len(versions1) else 0
+            v2 = versions2[i] if i < len(versions2) else 0
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+        return 0
+
+
+ob1 = Solution()
+
+# Get package version
+
+
+def appeears_version():
+    url = "https://pypi.org/project/appeears/"
+    source = requests.get(url)
+    html_content = source.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    company = soup.find("h1")
+    vcheck = ob1.compareVersion(
+        company.string.strip().split(" ")[-1],
+        pkg_resources.get_distribution("appeears").version,
+    )
+    if vcheck == 1:
+        print(
+            "\n"
+            + "========================================================================="
+        )
+        print(
+            "Current version of pyaqua is {} upgrade to lastest version: {}".format(
+                pkg_resources.get_distribution("appeears").version,
+                company.string.strip().split(" ")[-1],
+            )
+        )
+        print(
+            "========================================================================="
+        )
+    elif vcheck == -1:
+        print(
+            "\n"
+            + "========================================================================="
+        )
+        print(
+            "Possibly running staging code {} compared to pypi release {}".format(
+                pkg_resources.get_distribution("appeears").version,
+                company.string.strip().split(" ")[-1],
+            )
+        )
+        print(
+            "========================================================================="
+        )
+
+
+appeears_version()
 
 error_codes = {
     400: "Bad Request - The request could not be understood by the server due to malformed syntax.",
@@ -72,18 +134,17 @@ def tokenizer():
     )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     token_response = response.json()
     return token_response["token"]
 
 
 # get product list
 def products(keyword):
-    response = requests.get(
-        "https://appeears.earthdatacloud.nasa.gov/api/product")
+    response = requests.get("https://appeears.earthdatacloud.nasa.gov/api/product")
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     product_response = response.json()
     products = {p["ProductAndVersion"]: p for p in product_response}
     if keyword is not None:
@@ -93,8 +154,7 @@ def products(keyword):
                 (str(k).lower(), str(v).lower()) for k, v in product_dict.items()
             )
             if keyword.lower() in uniform_product_dict.values():
-                product_dict["product_id"] = product_dict.pop(
-                    "ProductAndVersion")
+                product_dict["product_id"] = product_dict.pop("ProductAndVersion")
                 print(json.dumps(product_dict, indent=2))
     else:
         for product in products:
@@ -114,7 +174,7 @@ def layers(pid):
     )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     layer_response = response.json()
     for layer in layer_response:
         print(layer)
@@ -126,8 +186,7 @@ def layers_from_parser(args):
 
 # get spatial projection info
 def spatial():
-    response = requests.get(
-        'https://appeears.earthdatacloud.nasa.gov/api/spatial/proj')
+    response = requests.get("https://appeears.earthdatacloud.nasa.gov/api/spatial/proj")
     proj_response = response.json()
     print(json.dumps(proj_response, indent=2))
 
@@ -136,17 +195,130 @@ def spatial_from_parser(args):
     spatial()
 
 
+# submit a task task_payload
+payload = {
+    "task_type": "",
+    "task_name": "",
+    "params": {
+        "layers": "",
+        "output": {
+            "format": {
+                "type": "geotiff",
+            },
+            "projection": "geographic",
+        },
+        "dates": [
+            {
+                "startDate": "",
+                "endDate": "",
+                "recurring": False,
+                "yearRange": [
+                    1950,
+                    2050,
+                ],
+            },
+        ],
+        "coordinates": "",
+    },
+}
+
+
+# submit a task
+def tasksubmit(**kwargs):
+    token = tokenizer()
+    for key, value in kwargs.items():
+        if key == "name":
+            payload["task_name"] = value
+        if key == "input":
+            testfile = pygeoj.load(value)
+            coordinates = []
+            for feature in testfile:
+                if feature.geometry.type == "Point":
+                    payload["task_type"] = "point"
+                    lat_long_dict = {
+                        "latitude": feature.geometry.coordinates[1],
+                        "longitude": feature.geometry.coordinates[0],
+                    }
+                    coordinates.append(lat_long_dict)
+                    payload["params"]["coordinates"] = coordinates
+                elif feature.geometry.type == "Polygon":
+                    payload["task_type"] = "area"
+                    geom = {
+                        "geo": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "properties": {},
+                                    "geometry": {
+                                        "type": "Polygon",
+                                        "coordinates": feature.geometry.coordinates,
+                                    },
+                                },
+                            ],
+                        },
+                    }
+                    coordinates = feature.geometry.coordinates
+                    payload["params"].pop("coordinates")
+                    payload["params"].update(geom)
+                else:
+                    sys.exit("Unknown geometry type")
+
+        if key == "start":
+            value = datetime.datetime.strptime(value, "%Y-%m-%d").strftime("%m-%d-%Y")
+            payload["params"]["dates"][0]["startDate"] = str(value)
+        if key == "end":
+            value = datetime.datetime.strptime(value, "%Y-%m-%d").strftime("%m-%d-%Y")
+            payload["params"]["dates"][0]["endDate"] = str(value)
+        if key == "recurring":
+            payload["params"]["dates"][0]["recurring"] = bool(value)
+        if key == "product":
+            response = requests.get(
+                f"https://appeears.earthdatacloud.nasa.gov/api/product/{value}"
+            )
+            layer_response = response.json()
+            layer_list = []
+            for layer in layer_response:
+                layer_json = {"layer": layer, "product": value}
+                layer_list.append(layer_json)
+            payload["params"]["layers"] = layer_list
+    # print(json.dumps(payload, indent=2))
+
+    response = requests.post(
+        "https://appeears.earthdatacloud.nasa.gov/api/task",
+        headers={"Authorization": f"Bearer {token}"},
+        json=payload,
+    )
+    if response.status_code in error_codes.keys():
+        print(error_codes.get(response.status_code))
+        sys.exit(response.json()["message"])
+    else:
+        print(f"Submitted task with task ID {response.json()['task_id']}")
+
+
+def tasksubmit_from_parser(args):
+    tasksubmit(
+        name=args.name,
+        product=args.product,
+        start=args.start,
+        end=args.end,
+        recurring=args.recurring,
+        input=args.geometry,
+    )
+
+
 # delete task using task id
 def delete(tid):
     token = tokenizer()
     response = requests.delete(
-        f'https://appeears.earthdatacloud.nasa.gov/api/task/{tid}',
-        headers={"Authorization": f"Bearer {token}"})
+        f"https://appeears.earthdatacloud.nasa.gov/api/task/{tid}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     elif response.status_code == 204:
-        print(f'Task with task id {tid} deleted')
+        print(f"Task with task id {tid} deleted")
 
 
 def delete_from_parser(args):
@@ -162,7 +334,7 @@ def task_all(status):
     )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     status_response = response.json()
     if len(status_response) != 0:
         task_list = []
@@ -219,7 +391,7 @@ def task_status(tid, status):
         )
         if response.status_code in error_codes.keys():
             print(error_codes.get(response.status_code))
-            sys.exit(response.json()['message'])
+            sys.exit(response.json()["message"])
         if response.status_code == 200:
             status_response = response.json()
             if (
@@ -234,7 +406,7 @@ def task_status(tid, status):
                 )
                 if response.status_code in error_codes.keys():
                     print(error_codes.get(response.status_code))
-                    sys.exit(response.json()['message'])
+                    sys.exit(response.json()["message"])
                 task_response = response.json()
                 if len(task_response) != 0:
                     for task in task_response:
@@ -269,7 +441,7 @@ def task_status(tid, status):
                         )
                         if response.status_code in error_codes.keys():
                             print(error_codes.get(response.status_code))
-                            sys.exit(response.json()['message'])
+                            sys.exit(response.json()["message"])
                         task_json = response.json()
                         for key, value in task_json.items():
                             if key == "progress":
@@ -296,7 +468,7 @@ def file_bundle(tid):
     )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     bundle_response = response.json()
     file_size_total = []
     file_id_list = []
@@ -304,8 +476,7 @@ def file_bundle(tid):
         file_id_list.append({file["file_id"]: file["file_name"]})
         file_size_total.append(file["file_size"])
     print(
-        "Estimated Download Size for order: {}".format(
-            humansize(sum(file_size_total)))
+        "Estimated Download Size for order: {}".format(humansize(sum(file_size_total)))
     )
     return natsorted(file_id_list)
 
@@ -320,7 +491,7 @@ def download_task(tid, dest_dir):
     )
     if response.status_code in error_codes.keys():
         print(error_codes.get(response.status_code))
-        sys.exit(response.json()['message'])
+        sys.exit(response.json()["message"])
     if response.status_code == 200:
         if "status" in response.json().keys():
             file_id_list = file_bundle(tid)
@@ -340,15 +511,13 @@ def download_task(tid, dest_dir):
                         os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
                         if not os.path.exists(filepath):
-                            print(
-                                f"Downloading {i} of {len(file_id_list)}: {filename}")
+                            print(f"Downloading {i} of {len(file_id_list)}: {filename}")
                             with open(filepath, "wb") as f:
                                 for data in response.iter_content(chunk_size=8192):
                                     f.write(data)
                             i = i + 1
                         else:
-                            print(
-                                f"File {filename} already exists. Skipping download")
+                            print(f"File {filename} already exists. Skipping download")
                             i = i + 1
         else:
             print(f"Task {tid} has not completed processing. Skipping download")
@@ -359,8 +528,7 @@ def download_from_parser(args):
 
 
 def main(args=None):
-    parser = argparse.ArgumentParser(
-        description="Simple CLI for NASA AppEEARS API")
+    parser = argparse.ArgumentParser(description="Simple CLI for NASA AppEEARS API")
     subparsers = parser.add_subparsers()
 
     parser_auth = subparsers.add_parser(
@@ -371,8 +539,7 @@ def main(args=None):
     parser_products = subparsers.add_parser(
         "products", help="Print product list for all products or keyword match"
     )
-    optional_named = parser_products.add_argument_group(
-        "Optional named arguments")
+    optional_named = parser_products.add_argument_group("Optional named arguments")
     optional_named.add_argument(
         "--keyword", help="Pass product keyword for example ecostress", default=None
     )
@@ -381,8 +548,7 @@ def main(args=None):
     parser_layers = subparsers.add_parser(
         "layers", help="Print layer list for product with Product ID"
     )
-    required_named = parser_layers.add_argument_group(
-        "Required named arguments.")
+    required_named = parser_layers.add_argument_group("Required named arguments.")
     required_named.add_argument(
         "--pid", help="Product ID from products tool", required=True
     )
@@ -393,12 +559,34 @@ def main(args=None):
     )
     parser_spatial.set_defaults(func=spatial_from_parser)
 
+    parser_tasksubmit = subparsers.add_parser("task-submit", help="Submit your task")
+    required_named = parser_tasksubmit.add_argument_group("Required named arguments.")
+    required_named.add_argument("--name", help="Task name", required=True)
+    required_named.add_argument(
+        "--product", help="Product ID returned from product tool", required=True
+    )
+    required_named.add_argument(
+        "--geometry",
+        help="Full path to geometry.geojson file point or single polygon",
+        required=True,
+    )
+    required_named.add_argument(
+        "--start", help="Start date in format YYYY-MM-DD", required=True
+    )
+    required_named.add_argument(
+        "--end", help="End date in format YYYY-MM-DD", required=True
+    )
+    optional_named = parser_tasksubmit.add_argument_group("Optional named arguments")
+    optional_named.add_argument(
+        "--recurring", help="Date range recurring True|False", default=False
+    )
+    parser_tasksubmit.set_defaults(func=tasksubmit_from_parser)
+
     parser_taskinfo = subparsers.add_parser(
-        "taskinfo",
+        "task-info",
         help="Get task information for all tasks or specific tasks or task status type",
     )
-    optional_named = parser_taskinfo.add_argument_group(
-        "Optional named arguments")
+    optional_named = parser_taskinfo.add_argument_group("Optional named arguments")
     optional_named.add_argument("--tid", help="Task ID", default=None)
     optional_named.add_argument(
         "--status", help="Task status processing|done|pending", default=None
@@ -408,19 +596,15 @@ def main(args=None):
     parser_delete = subparsers.add_parser(
         "delete", help="Delete a specific task with task ID"
     )
-    required_named = parser_delete.add_argument_group(
-        "Required named arguments.")
-    required_named.add_argument(
-        "--tid", help="Task ID to delete", required=True)
+    required_named = parser_delete.add_argument_group("Required named arguments.")
+    required_named.add_argument("--tid", help="Task ID to delete", required=True)
     parser_delete.set_defaults(func=delete_from_parser)
 
     parser_download = subparsers.add_parser(
         "download", help="Download all files for specific task with task ID"
     )
-    required_named = parser_download.add_argument_group(
-        "Required named arguments.")
-    required_named.add_argument(
-        "--tid", help="Task ID to download", required=True)
+    required_named = parser_download.add_argument_group("Required named arguments.")
+    required_named.add_argument("--tid", help="Task ID to download", required=True)
     required_named.add_argument(
         "--dest", help="Full path to destination directory", required=True
     )
@@ -434,6 +618,5 @@ def main(args=None):
         parser.error("too few arguments")
     func(args)
 
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ def readme():
 
 setuptools.setup(
     name="appeears",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     url="https://github.com/samapriya/appeears",
     install_requires=[
         "tqdm >= 4.56.0",
         "requests >= 2.28.1",
-        "natsort >= 8.1.0"
+        "natsort >= 8.1.0",
+        "beautifulsoup4>=4.11.1",
+        "PyGeoj==1.0.0"
     ],
     license="Apache 2.0",
     long_description=open("README.md").read(),
